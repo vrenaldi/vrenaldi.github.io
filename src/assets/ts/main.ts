1ff3e7ace14e15c217942fee5bdfedd01a6defd5
vrenaldi <vrenaldi258@gmail.com>
2017-11-14 09:58:56 +0100

    Beautify avatar and card-back (portrait), implement card-front (landscape), stop links' propagation.

--- src/assets/ts/main.ts
+++ src/assets/ts/main.ts
@@ -6,6 +6,7 @@ import { initSkills, loadSkills, clearSkills } from "./skills";
 import loadPortfolio from "./portfolio";
 
 let container;
+let links;
 
 function main(): void {
     loadAvatar();
@@ -18,10 +19,14 @@ function main(): void {
 
 function loadElements(): void {
     container = document.getElementById("container");
+    links = document.querySelectorAll("a");
 }
 
 function loadListeners(): void {
     container.addEventListener("click", conClicked);
+    links.forEach(element => {
+        element.addEventListener("click", e => { e.stopPropagation(); });
+    });
 }
 
 function conClicked(): void {
--- src/assets/ts/skills.ts
+++ src/assets/ts/skills.ts
@@ -10,9 +10,18 @@ let skillsInfo: { html: Text, css: Text, ts: Text };
 let skillsTitle: { html: Text, css: Text, ts: Text };
 
 let svg = Snap("#skills svg").attr({ viewBox: "0 0 100 50" });
-let neon = svg.filter(Snap.filter.shadow(0, 0, "#2196F3", 3.5));
 
 export function initSkills(): void {
+    skills = {
+        html: { class: "skills-chart", snapObj: {}, startX: 17, startY: 7, radius: 13, rotationX: 0, largeArc: 1, sweep: 1, endX: 17, endY: 7, strength: 0.8 },
+        css: { class: "skills-chart", snapObj: {}, startX: 50, startY: 7, radius: 13, rotationX: 0, largeArc: 1, sweep: 1, endX: 50, endY: 7, strength: 0.8 },
+        ts: { class: "skills-chart", snapObj: {}, startX: 83, startY: 7, radius: 13, rotationX: 0, largeArc: 1, sweep: 1, endX: 83, endY: 7, strength: 0.75 }
+    };
+    skillsInfo = {
+        html: { class: "skills-info", snapObj: {}, startX: 17, startY: 20, text: "75" },
+        css: { class: "skills-info", snapObj: {}, startX: 50, startY: 20, text: "75" },
+        ts: { class: "skills-info", snapObj: {}, startX: 83, startY: 20, text: "75" }
+    };
     skillsTitle = {
         html: { class: "skills-title", snapObj: {}, startX: 17, startY: 40, text: "HTML5" },
         css: { class: "skills-title", snapObj: {}, startX: 50, startY: 40, text: "CSS3" },
@@ -25,19 +34,13 @@ export function initSkills(): void {
     skillsTitle.css.snapObj.attr({ x: skillsTitle.css.startX - (skillsTitle.css.snapObj.getBBox().width / 2), y: skillsTitle.css.startY + (skillsTitle.css.snapObj.getBBox().height / 4) });
     skillsTitle.ts.snapObj = svg.text(skillsTitle.ts.startX, skillsTitle.ts.startY, skillsTitle.ts.text).attr({ class: skillsTitle.ts.class });
     skillsTitle.ts.snapObj.attr({ x: skillsTitle.ts.startX - (skillsTitle.ts.snapObj.getBBox().width / 2), y: skillsTitle.ts.startY + (skillsTitle.ts.snapObj.getBBox().height / 4) });
+
+    svg.circle(skills.html.startX, (skills.html.startY + skills.html.radius), skills.html.radius).attr({ class: "skills-chart-container" });
+    svg.circle(skills.css.startX, (skills.css.startY + skills.css.radius), skills.css.radius).attr({ class: "skills-chart-container" });
+    svg.circle(skills.ts.startX, (skills.ts.startY + skills.ts.radius), skills.ts.radius).attr({ class: "skills-chart-container" });
 }
 
 export function loadSkills(): void {
-    skills = {
-        html: { class: "skills-chart", snapObj: {}, startX: 17, startY: 7, radius: 13, rotationX: 0, largeArc: 1, sweep: 1, endX: 17, endY: 7, strength: 0.8 },
-        css: { class: "skills-chart", snapObj: {}, startX: 50, startY: 7, radius: 13, rotationX: 0, largeArc: 1, sweep: 1, endX: 50, endY: 7, strength: 0.8 },
-        ts: { class: "skills-chart", snapObj: {}, startX: 83, startY: 7, radius: 13, rotationX: 0, largeArc: 1, sweep: 1, endX: 83, endY: 7, strength: 0.75 }
-    };
-    skillsInfo = {
-        html: { class: "skills-info", snapObj: {}, startX: 17, startY: 20, text: "75" },
-        css: { class: "skills-info", snapObj: {}, startX: 50, startY: 20, text: "75" },
-        ts: { class: "skills-info", snapObj: {}, startX: 83, startY: 20, text: "75" }
-    };
 
     Snap.animate(0, skills.html.strength, function (val): void {
         if (skills.html.snapObj.hasOwnProperty("id")) {
@@ -50,7 +53,7 @@ export function loadSkills(): void {
         skills.html.largeArc = (val > 0.5) ? 1 : 0;
         skills.html.endX = skills.html.startX + (Math.cos(rad) * skills.html.radius);
         skills.html.endY = skills.html.startY + skills.html.radius + (Math.sin(rad) * skills.html.radius);
-        skills.html.snapObj = svg.path(`M ${skills.html.startX} ${skills.html.startY} A ${skills.html.radius} ${skills.html.radius} ${skills.html.rotationX} ${skills.html.largeArc} ${skills.html.sweep} ${skills.html.endX} ${skills.html.endY}`).attr({ class: skills.html.class, filter: neon });
+        skills.html.snapObj = svg.path(`M ${skills.html.startX} ${skills.html.startY} A ${skills.html.radius} ${skills.html.radius} ${skills.html.rotationX} ${skills.html.largeArc} ${skills.html.sweep} ${skills.html.endX} ${skills.html.endY}`).attr({ class: skills.html.class });
 
         skillsInfo.html.snapObj = svg.text(skillsInfo.html.startX, skillsInfo.html.startY, `${Math.round(val * 100)}`).attr({ class: skillsInfo.html.class });
         skillsInfo.html.snapObj.attr({ x: skillsInfo.html.startX - (skillsInfo.html.snapObj.getBBox().width / 2), y: skillsInfo.html.startY + (skillsInfo.html.snapObj.getBBox().height / 4) });
@@ -67,7 +70,7 @@ export function loadSkills(): void {
         skills.css.largeArc = (val > 0.5) ? 1 : 0;
         skills.css.endX = skills.css.startX + (Math.cos(rad) * skills.css.radius);
         skills.css.endY = skills.css.startY + skills.css.radius + (Math.sin(rad) * skills.css.radius);
-        skills.css.snapObj = svg.path(`M ${skills.css.startX} ${skills.css.startY} A ${skills.css.radius} ${skills.css.radius} ${skills.css.rotationX} ${skills.css.largeArc} ${skills.css.sweep} ${skills.css.endX} ${skills.css.endY}`).attr({ class: skills.html.class, filter: neon });
+        skills.css.snapObj = svg.path(`M ${skills.css.startX} ${skills.css.startY} A ${skills.css.radius} ${skills.css.radius} ${skills.css.rotationX} ${skills.css.largeArc} ${skills.css.sweep} ${skills.css.endX} ${skills.css.endY}`).attr({ class: skills.html.class });
 
         skillsInfo.css.snapObj = svg.text(skillsInfo.css.startX, skillsInfo.css.startY, `${Math.round(val * 100)}`).attr({ class: skillsInfo.css.class });
         skillsInfo.css.snapObj.attr({ x: skillsInfo.css.startX - (skillsInfo.css.snapObj.getBBox().width / 2), y: skillsInfo.css.startY + (skillsInfo.css.snapObj.getBBox().height / 4) });
@@ -84,7 +87,7 @@ export function loadSkills(): void {
         skills.ts.largeArc = (val > 0.5) ? 1 : 0;
         skills.ts.endX = skills.ts.startX + (Math.cos(rad) * skills.ts.radius);
         skills.ts.endY = skills.ts.startY + skills.ts.radius + (Math.sin(rad) * skills.ts.radius);
-        skills.ts.snapObj = svg.path(`M ${skills.ts.startX} ${skills.ts.startY} A ${skills.ts.radius} ${skills.ts.radius} ${skills.ts.rotationX} ${skills.ts.largeArc} ${skills.ts.sweep} ${skills.ts.endX} ${skills.ts.endY}`).attr({ class: skills.html.class, filter: neon });
+        skills.ts.snapObj = svg.path(`M ${skills.ts.startX} ${skills.ts.startY} A ${skills.ts.radius} ${skills.ts.radius} ${skills.ts.rotationX} ${skills.ts.largeArc} ${skills.ts.sweep} ${skills.ts.endX} ${skills.ts.endY}`).attr({ class: skills.html.class });
 
         skillsInfo.ts.snapObj = svg.text(skillsInfo.ts.startX, skillsInfo.ts.startY, `${Math.round(val * 100)}`).attr({ class: skillsInfo.ts.class });
         skillsInfo.ts.snapObj.attr({ x: skillsInfo.ts.startX - (skillsInfo.ts.snapObj.getBBox().width / 2), y: skillsInfo.ts.startY + (skillsInfo.ts.snapObj.getBBox().height / 4) });
