77afb0ac708d46fc6632f9c2513f2cf69f1c5e13
vrenaldi <vrenaldi258@gmail.com>
2018-10-06 17:13:04 +0200

    Update portfolio.

--- src/ts/portfolio.ts
+++ src/ts/portfolio.ts
@@ -1,10 +1,11 @@
+import * as spotifyExplorer from "../assets/images/spotify-explorer.jpg";
 import * as transformations from "../assets/images/transformations.jpg";
 import * as comingSoon from "../assets/images/comingSoon.jpg";
 
 export default function loadPortfolio():void{
     let projects = document.querySelectorAll(".portfolio-item img");
 
-    projects[0].setAttribute("src", <any>transformations);
-    projects[1].setAttribute("src", <any>comingSoon);
+    projects[0].setAttribute("src", <any>spotifyExplorer);
+    projects[1].setAttribute("src", <any>transformations);
     projects[2].setAttribute("src", <any>comingSoon);
 }
